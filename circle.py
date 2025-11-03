2ed26037da1ccbd3a3b65114418dfe60241d2c05
robiand <anderssonrobin034@gmail.com>
2025-11-03 16:43:30 +0100

    created files

---
+++ circle.py
@@ -0,0 +1,5 @@
+class circle:
+    def __init__(self, x: int | float, y: int | float, radius: int | float):
+        self.x = x
+        self.y = y
+        self.radius = radius
---
+++ rectangle.py
@@ -0,0 +1,7 @@
+class rectangle:
+    def __init__(self, x: int | float, y: int | float, width: int | float, height: int | float):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        
