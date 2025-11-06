61cf4c27f32f204ee6b62f7f21e86a5aff095181
robiand <anderssonrobin034@gmail.com>
2025-11-06 20:34:39 +0100

    new file structure, shapes now inherit from "shape", start on overloads

--- circle.py
+++ circle.py
@@ -1,5 +1,10 @@
+from shape import Shape
+
 class circle:
     def __init__(self, x: int | float, y: int | float, radius: int | float):
         self.x = x
         self.y = y
         self.radius = radius
+
+    def __repr__(self):
+        return f"Circle, X-pos: {self.x}, Y-Pos: {self.y}, Radius: {self.radius}"
--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,11 @@
+from shape import Shape
+
 class rectangle:
     def __init__(self, x: int | float, y: int | float, width: int | float, height: int | float):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        
+    
+    def __repr__(self):
+        return f"Rectangle, X-pos: {self.x}, Y-Pos: {self.y}, width: {self.width}, height: {self.height}"  
