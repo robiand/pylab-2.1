b34f723ae089c7dfd37d88268803664567819921
robiand <anderssonrobin034@gmail.com>
2025-11-06 21:06:07 +0100

    updated rectangle

--- rectangle.py
+++ rectangle.py
@@ -6,6 +6,14 @@ class rectangle:
         self.y = y
         self.width = width
         self.height = height
+        self.area = self.width * self.height
     
+
     def __repr__(self):
         return f"Rectangle, X-pos: {self.x}, Y-Pos: {self.y}, width: {self.width}, height: {self.height}"  
+    
+    def __eq__(self, value): #checks rectangle area to see if both rectangles are of equal size
+        print(self.area)
+
+    def __lt__(self, other: Shape) -> None:
+        print(self < other)
--- shape.py
+++ shape.py
@@ -4,3 +4,7 @@ class shape:
     def __init__(self, x: int | float, y: int | float): #Each shape will have x and y pos values
         self.x = x
         self.y = y
+
+    def translate(self, x, y): #used to "move" shapes by changing their x and y values
+        self.x += x
+        self.y += y
